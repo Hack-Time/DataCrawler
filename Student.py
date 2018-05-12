ab518e0401a9ce1e579ea56aac727430b956882a
kingcos <2821836721v@gmail.com>
2018-05-12 17:51:20 +0800

    Refctor & parse successfully

--- Student.py
+++ Student.py
@@ -4,6 +4,6 @@ class Student:
     id = 0
     subjects = []
 
-    def __init__(self, id, subjects):
-        self.id = id
-        self.subjects = subjects
+    # def __init__(self, id, subjects):
+    #     self.id = id
+    #     self.subjects = subjects
